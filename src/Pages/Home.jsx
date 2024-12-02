25221dd4a20bd34abf30ddf1216869f26b718422
--global <--global>
2024-12-03 01:57:51 +0530

    removed Faq from home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,7 +34,6 @@ export default function home() {
       <DevProcess />
       {/* <CaseStudies /> */}
       <HomeIndustries />
-      {/* <FAQs /> */}
       <ProjectForm />
       <Footer />
     </>
